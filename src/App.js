2b2fcc1f0d44fdcc8ca330dd028335a90c1fe7a0
MaalemAbdelmouaaz <maalemabdelmouaaz@gmail.com>
2022-07-31 21:11:28 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,56 +1,73 @@
-import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import React, { useCallback, useState } from "react";
+import { Calendar, Views, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
-
-import "./App.css";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import events from "./events";
 
 const localizer = momentLocalizer(moment);
-const DnDCalendar = withDragAndDrop(Calendar);
+const DragAndDropCalendar = withDragAndDrop(Calendar);
 
-class App extends Component {
-  state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment().add(1, "days").toDate(),
-        title: "Some title",
-      },
-    ],
-  };
+export default function DragAndDrop() {
+  const [myEvents, setMyEvents] = useState(events);
 
-  onEventResize = (data) => {
-    const { start, end } = data;
+  const handleSelectSlot = useCallback(
+    ({ start, end }) => {
+      const title = window.prompt("New Event name");
+      if (title) {
+        setMyEvents((prev) => [...prev, { start, end, title }]);
+      }
+    },
+    [setMyEvents]
+  );
 
-    this.setState((state) => {
-      state.events[0].start = start;
-      state.events[0].end = end;
-      return { events: [...state.events] };
-    });
-  };
+  const handleSelectEvent = useCallback(
+    (event) => window.alert(event.title),
+    []
+  );
 
-  onEventDrop = (data) => {
-    console.log(data);
-  };
+  const moveEvent = useCallback(
+    ({ event, start, end, isAllDay: droppedOnAllDaySlot = false }) => {
+      const { allDay } = event;
+      if (!allDay && droppedOnAllDaySlot) {
+        event.allDay = true;
+      }
 
-  render() {
-    return (
-      <div className="App">
-        <DnDCalendar
-          defaultDate={moment().toDate()}
-          defaultView="month"
-          events={this.state.events}
-          localizer={localizer}
-          onEventDrop={this.onEventDrop}
-          onEventResize={this.onEventResize}
-          resizable
-          style={{ height: "100vh" }}
-        />
-      </div>
-    );
-  }
+      setMyEvents((prev) => {
+        const existing = prev.find((ev) => ev.id === event.id) ?? {};
+        const filtered = prev.filter((ev) => ev.id !== event.id);
+        return [...filtered, { ...existing, start, end, allDay }];
+      });
+    },
+    [setMyEvents]
+  );
+
+  const resizeEvent = useCallback(
+    ({ event, start, end }) => {
+      setMyEvents((prev) => {
+        const existing = prev.find((ev) => ev.id === event.id) ?? {};
+        const filtered = prev.filter((ev) => ev.id !== event.id);
+        return [...filtered, { ...existing, start, end }];
+      });
+    },
+    [setMyEvents]
+  );
+
+  return (
+    <DragAndDropCalendar
+      defaultView={Views.WEEK}
+      events={myEvents}
+      localizer={localizer}
+      onEventDrop={moveEvent}
+      onEventResize={resizeEvent}
+      onSelectSlot={handleSelectSlot}
+      min={new Date(0, 0, 0, 8, 0, 0)}
+      max={new Date(0, 0, 0, 23, 0, 0)}
+      selectable
+      popup
+      resizable
+      style={{ height: "820px" }}
+    />
+  );
 }
-
-export default App;
